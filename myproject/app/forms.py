2c8fe932b1e6cb7047b3fcc17697f3122abcd3f1
Marko-Shevchuk <markoliubomirshevchykwork@gmail.com>
2024-01-15 15:10:57 +0200

    Implemented flash messages, wtf forms, fixed header to use a loop

--- myproject/app/forms.py
+++ myproject/app/forms.py
@@ -3,21 +3,27 @@ from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length
 class LoginForm(FlaskForm):
     login = StringField("Login",
-                        render_kw={"placeholder": ""},
+                        render_kw={"placeholder": "Login"},
                         validators=[
-                            DataRequired(message="Your login cannot be empty, ██████.")
+                            DataRequired(message="Your login cannot be empty.")
                         ])
     password = PasswordField("Password",
-                             render_kw={"placeholder": ""},
+                             render_kw={"placeholder": "Password"},
                              validators=[
                                  DataRequired(message="Your password cannot be empty."),
                                  Length(min=4, max=10)
                              ])
     remember = BooleanField("Remember", default=False)
     submit = SubmitField("Sign in")
-class PasswordChangeForm(FlaskForm):
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField("Old Password",
+                                 render_kw={"placeholder": "New Password"},
+                                 validators=[
+                                     DataRequired(message="Password cannot be empty."),
+                                     Length(min=4, max=10)
+                                 ])
     new_password = PasswordField("New Password",
-                                 render_kw={"placeholder": ""},
+                                 render_kw={"placeholder": "New Password"},
                                  validators=[
                                      DataRequired(message="Password cannot be empty."),
                                      Length(min=4, max=10)
