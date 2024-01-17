ef2335c1c7554a46f4f0aa54fe61c84069c5bf24
Marko-Shevchuk <markoliubomirshevchykwork@gmail.com>
2024-01-17 14:04:51 +0200

    Added database flask-sqlalchemy

--- myproject/app/__init__.py
+++ myproject/app/__init__.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
 app.secret_key = "pleasedosha512fiwkeokweowoefkm3r8j"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db.init_app(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String())
+    password = db.Column(db.String())
 
 from app import views
