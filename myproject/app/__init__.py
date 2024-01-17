cbdd611c737a832dd8759fb59875cd59898c0ae7
Marko-Shevchuk <markoliubomirshevchykwork@gmail.com>
2024-01-17 15:19:06 +0200

    Added migrations

--- myproject/app/__init__.py
+++ myproject/app/__init__.py
@@ -1,15 +1,22 @@
 from flask import Flask
+
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-app = Flask(__name__)
-app.secret_key = "pleasedosha512fiwkeokweowoefkm3r8j"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = "pleasedosha512fiwkeokweowoefkm3r8j"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    return app
 
-db.init_app(app)
+app=create_app()
+Migrate(app, db)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
