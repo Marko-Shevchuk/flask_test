6e888dc8aed3fd5c5ee982aea0dba42ee503ed62
Marko-Shevchuk <markoliubomirshevchykwork@gmail.com>
2024-01-18 05:39:24 +0200

    Connected to render database

--- myproject/app/__init__.py
+++ myproject/app/__init__.py
@@ -4,12 +4,12 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
+#postgres://base_demo_user:XJXY08jGxhJ94h6aYn4ohZL5u1xpslTW@dpg-cmk9g2q1hbls739udos0-a.frankfurt-postgres.render.com/base_demo
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = "pleasedosha512fiwkeokweowoefkm3r8j"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://base_demo_user:XJXY08jGxhJ94h6aYn4ohZL5u1xpslTW@dpg-cmk9g2q1hbls739udos0-a.frankfurt-postgres.render.com/base_demo'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     return app
