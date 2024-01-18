da90c6c262e1fb9d9ab62d4ab28389e223f37c9d
Marko-Shevchuk <markoliubomirshevchykwork@gmail.com>
2024-01-18 07:04:04 +0200

    Fixed domain imports

--- myproject/app/forms.py
+++ myproject/app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, TextAreaField, SelectField, RadioField, EmailField, DateField, FileField
 from wtforms.validators import DataRequired, Length, EqualTo, ValidationError, Regexp
-from app.domain.Todo import Status
-from app.domain.Feedback import Satisfaction
-from app.domain.User import User
+from app.domain.Todo import *
+from app.domain.Feedback import *
+from app.domain.User import *
 class LoginForm(FlaskForm):
     login = StringField("Login",
                         render_kw={"placeholder": "Login"},
