ef7a609a42bb02c7b45231e16cfabec01cecad87
Marko-Shevchuk <markoliubomirshevchykwork@gmail.com>
2024-01-18 18:26:57 +0200

    Organisation, added config, began splitting into blueprints

--- myproject/app/__init__.py
+++ myproject/app/__init__.py
@@ -3,15 +3,17 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+from config import Config
+
 db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = "pleasedosha512fiwkeokweowoefkm3r8j"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    profile = Config.get_profile()
+    app.config.from_object(profile)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
+    db.init_app(app) #
     return app
 
 app=create_app()
@@ -19,5 +21,21 @@ Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+with app.app_context():
+    from .user import user_bp
+    from .cookie import cookie_bp
+    from .feedback import feedback_bp
+    from .todo import todo_bp
+    from .general import general_bp
+    from .auth import auth_bp
+
+    
+    app.register_blueprint(user_bp)
+    app.register_blueprint(cookie_bp)
+    app.register_blueprint(feedback_bp)
+    app.register_blueprint(todo_bp)
+    app.register_blueprint(general_bp)
+    app.register_blueprint(auth_bp)
 
 from app import views
